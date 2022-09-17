52a5bd2ad19a6cfdad4c559ac1302d6047e8dbbb
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-09-17 20:06:01 +0400

    adding password_length variable

--- passwordChercker.py
+++ passwordChercker.py
@@ -10,4 +10,8 @@ username = input('What is your username ?')
 
 password = input('what is your password')
 
+password_length = len(password)
+
+hidden_password = '*' * password_length
+
 print(f'{username} , your password , {password} is {len(password)} letters long')
